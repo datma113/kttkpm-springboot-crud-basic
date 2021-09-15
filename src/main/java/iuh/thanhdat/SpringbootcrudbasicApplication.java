ddc3fd27aac5153121db5b1fc580332980dcac6e
datma <datma113112111@gmail.com>
2021-09-15 10:55:35 +0700

    initial

---
+++ src/main/java/iuh/thanhdat/SpringbootcrudbasicApplication.java
@@ -0,0 +1,13 @@
+package iuh.thanhdat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrudbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/thanhdat/ThanhdatApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.thanhdat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanhdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
